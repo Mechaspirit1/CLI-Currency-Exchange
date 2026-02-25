d8d80e52d09fc69eb05a58cd40939513dda6676c
Mechaspirit1 <pabloloschi200@gmail.com>
2026-02-25 11:40:42 -0300

    refactoring for more usable code

--- curex.py
+++ curex.py
@@ -1,72 +1,73 @@
 #!/usr/bin/env python3
 import requests
 
-#modos diferentes do applicativo dependendo da necessidade
-#printa todos os codigos monetarios e suas taxas de cambio relativas a moeda usada como base
-def rate_list():
-    base_currency = input("Currency to base exchange rates: ").upper()
+#retorna a lista de codigos monetarios baseados na moeda base
+def rate_list(base_currency):
+    response = requests.get(f"https://open.er-api.com/v6/latest/{base_currency}")
+    resParsed = response.json()
+    currency_list = ''
 
-    try:
-        response = requests.get(f"https://open.er-api.com/v6/latest/{base_currency}")
-        resParsed = response.json()
+    for currency, rate in resParsed['rates'].items():
+        currency_list+=(f"{currency}: {round(rate, 2)}" + "\n")
 
-        for currency, rate in resParsed['rates'].items():
-            print(f"{currency}: {round(rate, 2)}")
+    return currency_list
 
-    except KeyError:
-        print("Invalid currency code")
-    except requests.RequestException:
-        print("Network/API error")
+#converte uma quantia de uma moeda para outra, arredonda os decimais e retorna o resultado
+def currency_exchange(primary_currency, secondary_currency, amount):
+    response = requests.get(f"https://open.er-api.com/v6/latest/{primary_currency}")
+    resParsed = response.json()
+    rate = resParsed['rates'][secondary_currency]
+    conversion = amount*rate
 
-#faz a conversão de uma quantia de dinheiro baseado nos codigos monetarios
-def currency_exchange():
-    primary_currency = input("Currency to be converted from: ").upper()
-    secondary_currency = input("Currency to be converted to: ").upper()
-    amount = float(input("Value to be converted: "))
+    return(round(conversion, 2))
 
-    try:
-        response = requests.get(f"https://open.er-api.com/v6/latest/{primary_currency}")
-        resParsed = response.json()
-        rate = resParsed['rates'][secondary_currency]
-        conversion = amount*rate
+def main():
+    banner = r"""
+    ____ _   _ ____  _______  __
+    / ___| | | |  _ \| ____\ \/ /
+    | |   | | | | |_) |  _|  \  /
+    | |___| |_| |  _ <| |___ /  \
+    \____|\___/|_| \_\_____/_/\_\
+    """
 
-        print(f"Current exchange rate: {secondary_currency}:{round(rate, 2)}")
-        print(f"Value conversion: {secondary_currency}:{round(conversion, 2)}")
+    print(banner)
+    print("A CLI currency exchange tool by Pablo Loschi | Powered by the ExchangeRate-API")
 
-    except KeyError:
-        print("Invalid currency code")
-    except requests.RequestException:
-        print("Network/API error")
+    while True:
+        print("1-Print currency list and rates | 2-Convert between currencies | 3-Exit")
+        try:
+            start = int(input())
+            
+            if start == 1:
+                try:
+                    base = input("Currency to base exchange rates: ").upper()
+                    print(rate_list(base))
+                except KeyError:
+                    print("Invalid currency code")
+                except requests.RequestException:
+                    print("Network/API error")
 
+            elif start == 2:
+                try:
+                    base = input("Currency to base exchange rates: ").upper()
+                    sec_base = input("Coversion currency: ").upper()
+                    amount = int(input("Ammount to be converted: "))
 
-banner = r"""
-  ____ _   _ ____  _______  __
- / ___| | | |  _ \| ____\ \/ /
-| |   | | | | |_) |  _|  \  /
-| |___| |_| |  _ <| |___ /  \
- \____|\___/|_| \_\_____/_/\_\
-"""
+                    print(f"{sec_base} {currency_exchange(base, sec_base, amount)}")
+                except KeyError:
+                    print("Invalid currency code")
+                except requests.RequestException:
+                    print("Network/API error")
 
-print(banner)
-print("A CLI currency exchange tool by Pablo Loschi | Powered by the ExchangeRate-API")
+            elif start == 3:
+                break
 
-while True:
-    print("1-Print currency list and rates | 2-Convert between currencies | 3-Exit")
+            else:
+                print("Invalid input !")
 
-    try:
-        start = int(input())
+        except ValueError:
+            print("Input only accepts numbers !\n")
+            continue
 
-        if start == 1:
-            rate_list()
-            print("")
-        elif start == 2:
-            currency_exchange()
-            print("")
-        elif start == 3:
-            break
-        else:
-            print("Invalid input !")
-
-    except ValueError:
-        print("Input only accepts numbers !\n")
-        continue
+if __name__ == "__main__":
+    main()
